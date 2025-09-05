b63cb3421b51ca966bf0f32c93994f5ab8c4da5f
Sriram3101 <ksriram3101@gmail.com>
2025-09-05 11:41:05 +0530

    Update Insertion.cpp

--- Linked_List/Insertion.cpp
+++ Linked_List/Insertion.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
+using namespace std;
+class NODE 
+{   
+    public:
+    int data;
+    NODE* next;
+    NODE (int value)
+    {
+        data = value;
+        next = NULL;
+    }
+};
 
+
+int count = 0;
+NODE* RecursiveInsertEnd(int arr[],int index,int n) {
+    if (index == n) {
+        return NULL;
+    }
+    else {
+        NODE* temp;
+        temp = new NODE(arr[index]);
+        temp->next = RecursiveInsertEnd(arr,index+1,n);
+        return temp; 
+    }
+}
+void DisplayLL(NODE* head) {
+    NODE*temp = head;
+    while (temp!=NULL) {
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+}
+
+
+int main() {
+    int arr[5] = {1,2,3,4,5};
+    NODE *p = RecursiveInsertEnd(arr,0,5);
+    DisplayLL(Deletefirst(p));
+    
+}
